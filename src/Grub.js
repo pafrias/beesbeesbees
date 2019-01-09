f4ae4ecde05c73c5d0ac096ce4fb2d77d2d5ed90
Pablo A Frias <pafrias90@gmail.com>
2019-01-09 12:28:44 -0800

    Pseudoclassical Bees completed

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,16 @@
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+Grub.prototype.eat = function() {
+};
+
+/*
+var Grub = function(age = 0, color = 'pink', food = 'jelly') {
+  this.age = age;
+  this.color = color;
+  this.food = food;
+};
+*/
